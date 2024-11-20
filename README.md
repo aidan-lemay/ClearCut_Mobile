8b06d49572b32602543fe82863a49f551689264a
Aidan LeMay <aquader14@gmail.com>
2024-11-20 10:20:21 -0500

    Adding and updating Info page

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Future Plans:
 * Settings for font size, dark/light mode, 12 vs 24 hour time, etc
-* Add an "About" and "Bug Report" page
+* ~~Add an "About" and "Bug Report" page~~ Done!
 * "Pull Down to Refresh" calls page
 * Add a way to get from the "Stream" page back to the "Favorites" or "Systems" without going back three levels
+* If playing, play button should stop playback
+* Timestamp should have date
+* "Load More" button under last call in list
+* "Advanced Search" page
+* Keep last position on app start
+* Fix the thing where it refreshes every 5 seconds, figure out how to properly stream data
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:collection/collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
 
 /* Clearcut API Paths:
 Audio: https://audio.clearcutradio.app/audio/us-ny-monroe/1077/us-ny-monroe-1077-1732038152.m4a
@@ -168,6 +170,8 @@ class _MySystemsPageState extends State<MySystemsPage> {
         page = SystemsPage();
       case 1:
         page = FavoritesPage();
+      case 2:
+        page = InfoPage();
       default:
         throw UnimplementedError('No widget for $selectedIndex');
     }
@@ -214,6 +218,16 @@ class _MySystemsPageState extends State<MySystemsPage> {
               onTap: () {
                 setState(() {
                   selectedIndex = 1;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text('Info'),
+              onTap: () {
+                setState(() {
+                  selectedIndex = 2;
                 });
                 Navigator.pop(context);
               },
@@ -766,6 +780,98 @@ class FavoritesPage extends StatelessWidget {
               icon: Icon(Icons.delete)),
         );
       },
+    );
+  }
+}
+
+class InfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var github = Uri.parse("https://github.com/aidan-lemay/ClearCut_Mobile");
+    var mysite = Uri.parse("https://aidanlemay.com");
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path:
+          'clearcutfeedback@aidanlemay.com', // Replace with your email address
+      query: Uri.encodeQueryComponent(
+          'subject=Clearcut Mobile Feedback Request'), // Optional subject and body
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center, // Center content vertically
+      children: [
+        Text(
+          "Welcome to Clearcut Mobile!",
+          style: TextStyle(fontSize: 32),
+          textAlign: TextAlign.center,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            "Created by Aidan LeMay",
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              if (await canLaunchUrl(github)) {
+                await launchUrl(github);
+              } else {
+                await Clipboard.setData(ClipboardData(text: github.toString()));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                        "Couldn't open link, copied to clipboard instead!"),
+                  ),
+                );
+              }
+            },
+            child: Text('Check Out This Project on GitHub!'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              if (await canLaunchUrl(mysite)) {
+                await launchUrl(mysite);
+              } else {
+                await Clipboard.setData(ClipboardData(text: mysite.toString()));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                        "Couldn't open link, copied to clipboard instead!"),
+                  ),
+                );
+              }
+            },
+            child: Text('Check Out My Website!'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              if (await canLaunchUrl(emailUri)) {
+                await launchUrl(emailUri);
+              } else {
+                await Clipboard.setData(
+                    ClipboardData(text: emailUri.toString()));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "Couldn't open email app, copied address to clipboard instead!",
+                    ),
+                  ),
+                );
+              }
+            },
+            child: Text('Have Feedback? Email Me!'),
+          ),
+        ),
+      ],
     );
   }
 }
